e8dcf3a31868bdd34fe803cbfbac62e85620b66c
Masoud PourZahmatkesh <mpzadmin@gmail.com>
2020-12-14 09:21:33 +0330

    Add a few update on test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
     int number = 10;
+    float number2 = 100;
     cout << "Hello";
     cout << "This is C++";    
 }
